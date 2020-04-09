b78067cdb43b5d88badf3be9426b9b46d2169e0d
taylorgao <taylorgao@163.com>
2020-04-09 23:08:36 +0800

    b

--- src/com/gbs/thread/condition/Quee.java
+++ src/com/gbs/thread/condition/Quee.java
@@ -21,8 +21,11 @@ public class Quee<T> implements IQuee<T> {
     public void put(T x) throws InterruptedException {
         lock.lock();
         try {
-            while(quee.size() == limit)
+            while(quee.size() == limit) {
+                System.out.println(Thread.currentThread().getName() + ",put is waiting");
                 notFull.await();
+            }
+            System.out.println(Thread.currentThread().getName() + ",put " + x);
 
             quee.add(x);
             notEmpty.signal();
@@ -35,6 +38,51 @@ public class Quee<T> implements IQuee<T> {
 
     @Override
     public T take() throws InterruptedException {
-        return null;
+        lock.lock();
+        T t = null;
+        try {
+            while(quee.isEmpty()){
+                System.out.println(Thread.currentThread().getName() + ",take is waiting");
+                notEmpty.await();}
+            t = (T)quee.remove(0);
+            notFull.signal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+        return t;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Quee<String> quee = new Quee<>(4);
+
+            new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    try {
+                        for(int j = 1; j < 10; j ++)
+                        quee.put("good" + j);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
+            new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    try {
+                        for (int i = 0; i < 10; i++) {
+
+                            String s = quee.take();
+                            System.out.println(s);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
     }
 }
